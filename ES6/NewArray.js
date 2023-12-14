a06891e9f9eddbe6d50ff2ff22aba32a7eece646
johnsondas <johnsondas4342@gmail.com>
2023-12-14 15:10:57 +0530

    es6

---
+++ ES6/NewArray.js
@@ -0,0 +1,22 @@
+let ids=[101,102,103,104,]
+ids.map(()=>{
+     console.log("gm")
+}) 
+ 
+console.log("***************")
+
+ids.forEach(()=>{
+    console.log("gm")
+})
+
+console.log("************")
+
+ids.filter(()=>{
+    console.log("gm")
+})
+
+console.log("**********")
+
+ids.reduce(()=>{
+    console.log("gm")
+})
